7c298c4ac3a70f0885970582e40540ceefb6ca26
Austin <ahalbritter@hotmail.com>
2021-04-14 20:40:46 -0400

    This is the placeholder front end for the header-footer-home page- & nav bar

--- grace-shopper-unf/src/App.js
+++ grace-shopper-unf/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from "./Header";
+import Home from "./Home";
+import FeaturedProducts from "./FeaturedProducts";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Route exact path='/'>
+          <Home />
+          < FeaturedProducts />
+          < Footer />
+        </Route>
+
+        <Route exact path='/products'>
+        </Route>
+
+        <Route exact path='/about'>
+        </Route>
+
+        <Route exact path='/contact'>
+        </Route>
+
+        <Route exact path='/account'>
+        </Route>
+      </Router>
     </div>
   );
 }
